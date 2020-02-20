379c12d2a9affa270c1053d2a0e91adec4ccf214
Dennis Macharia <dennismacharia4747@gmail.com>
2020-02-20 11:53:42 +0300

    add Article model add get articles route

--- main.go
+++ main.go
@@ -1,18 +1,36 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
 
+// Article model
+type Article struct {
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Content     string `json:"content"`
+}
+
+// Articles array
+type Articles []Article
+
 func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Homepage endpoint HIT:")
+	fmt.Fprint(w, "Endpoint HIT: Homepage")
+}
+
+func getArticles(w http.ResponseWriter, r *http.Request) {
+	articles := Articles{Article{Title: "Welcome", Description: "Welcome Description", Content: "Hello world"}}
+
+	fmt.Println("Endpoint HIT: Get all articles")
+	json.NewEncoder(w).Encode(articles)
 }
 
 func handleRequests() {
 	http.HandleFunc("/", homePage)
-
+	http.HandleFunc("/articles", getArticles)
 	log.Fatal(http.ListenAndServe(":8081", nil))
 }
 
